f1948f20d2be73c541a4a01b1fe744fb19053427
root <root@ip-172-31-8-132.us-east-2.compute.internal>
2021-07-06 17:58:00 +0000

    Added basic scripts for practice

---
+++ basicScripts/caseOperations.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo -e "***Calculator***\nChoose an option from the below list:\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division"
+read option
+if [ $option -gt 4 ];then
+	echo "Invalid option!! Choose a correct option "
+	exit
+fi
+echo "Enter first number:"
+read a
+echo  "Enter second number:"
+read b
+result=0
+case $option in
+'1')
+	result=`expr $a + $b`
+	echo "Addition of Numbers is : $result"
+	;;
+'2')
+	result=`expr $a - $b`
+	echo "Subtraction of Numbers is: $result"
+	;;
+'3')
+	result=`expr $a \* $b`
+	echo "Multiplication of Numbers is: $result"
+	;;
+'4')
+	result=`expr $a / $b`
+	echo "Division of numbers is: $result"
+esac
+
